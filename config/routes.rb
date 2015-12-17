8dad1b6611b3345a6be7cc57ebcc168bd0923da9
Rommel Toledo <rommeltoledo@gmail.com>
2015-12-17 11:55:05 -0600

    realizados los modelos y controladores para personas, usuarios y tipos de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :usuarios
+  resources :tipo_usuarios
+  resources :personas
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
