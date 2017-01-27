1682874e4d9087042fa5d410025f13f7c55b430b
Javier Vazquez <jvazquezolivares@gmail.com>
2017-01-27 17:34:06 -0600

    Se anadieron controladores para la autentificacion de usuarios y se creo un nuevo layout para la autentificacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  layout :layout_by_resource
   protect_from_forgery with: :exception
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  #devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
+
   resources :bancos
   resources :proveedores
   get 'inventarios/index'
