8f6a8f2b739a7bbe9e96eccb283007e0be042f2a
Javier Vazquez <jvazquezolivares@gmail.com>
2017-03-06 18:57:24 -0600

    Se quito jQuery.print de initializers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require jQuery.print/jQuery.print.js
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require dataTables/extras/dataTables.responsive
 //= require dataTables/jquery.dataTables
-//= require jQuery.print/jQuery.print.js
 //= require turbolinks
 
