f2b868bfd2473cb665929be39e972c9a88bd61ce
Javier Vazquez <jvazquezolivares@gmail.com>
2017-03-19 21:58:49 -0600

    Se anadieron validaciones para los datos fiscales de los clientes

--- app/models/datos_fiscales_cliente.rb
+++ app/models/datos_fiscales_cliente.rb
@@ -1,3 +1,5 @@
 class DatosFiscalesCliente < ActiveRecord::Base
 	belongs_to :cliente
+
+	validates :nombreFiscal, :rfc, :calle, :numExterior, :numInterior, :colonia, :codigo_postal, :municipio, :delegacion, :estado, :email, :presence => { message: "Campo obligatorio"}
 end
