0835f0c84e46b6300f5517163bbe082989f18a10
Javier Vazquez <jvazquezolivares@gmail.com>
2017-03-24 20:21:48 -0600

    Correccion de error en modelo de entrada almacen

--- app/models/entrada_almacen.rb
+++ app/models/entrada_almacen.rb
@@ -2,6 +2,6 @@ class EntradaAlmacen < ActiveRecord::Base
 	belongs_to :compra
 	belongs_to :articulo
 
-	validates :cantidad, :fecha, :isEntradaInicial, :presence => { :message "Campos necesarios" }
+	validates :cantidad, :fecha, :isEntradaInicial, :presence => { message: "Campos necesarios" }
 	validates :cantidad, :numericality => { message: "campo numérico" }
 end
