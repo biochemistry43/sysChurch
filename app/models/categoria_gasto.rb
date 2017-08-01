d501e3cba03af54dc438f78d00d7aec93dbabc16
Javier Vazquez <jvazquezolivares@gmail.com>
2017-08-01 00:42:20 -0500

    Se corrigio el error de que no se pueden actualizar categorias de gastos y se permite la visualizacion de categorias principales pero no se permite su edicion

--- app/models/categoria_gasto.rb
+++ app/models/categoria_gasto.rb
@@ -2,6 +2,6 @@ class CategoriaGasto < ActiveRecord::Base
 	belongs_to :negocio
 	has_many :gastos
 
-	validates :nombreCategoria, :presence => { message: "La categoria debe tener un nombre" }
-	validates :nombreCategoria, uniqueness: { scope: :negocio_id, message: "Esta categoría ya existe" }
+	validates :nombre_categoria, :presence => { message: "La categoria debe tener un nombre" }
+	validates :nombre_categoria, uniqueness: { scope: :negocio_id, message: "Esta categoría ya existe" }
 end
