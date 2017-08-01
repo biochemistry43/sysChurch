4c511c153dc901a9bb178d55a92741d08ea194f2
Javier Vazquez <jvazquezolivares@gmail.com>
2017-08-01 02:18:36 -0500

    Se quito la validacion de nombre unico en la categoria de gasto

--- app/models/categoria_gasto.rb
+++ app/models/categoria_gasto.rb
@@ -3,5 +3,4 @@ class CategoriaGasto < ActiveRecord::Base
 	has_many :gastos
 
 	validates :nombre_categoria, :presence => { message: "La categoria debe tener un nombre" }
-	validates :nombre_categoria, uniqueness: { scope: :negocio_id, message: "Esta categoría ya existe" }
 end
