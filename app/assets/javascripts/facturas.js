63f66c0eedd10f8bb93ac8f46d843fae1dde988c
Novato <dani-elorenzo95@hotmail.com>
2018-05-26 23:54:21 -0500

    Se muestran u ocultan campos en el filtro de clientes con jQuery y validación de RFC cuando se seleccione bsucar por RFC

--- app/assets/javascripts/facturas.js
+++ app/assets/javascripts/facturas.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
     $("#filtro_por_cliente").click(function(e){
-      $('.rfc_input').hide().val("");
+      
+      if ($("#rbtn_rbtn_nombreFiscal").is(":checked")) {
+        $('#errorRFC').removeClass(" has-error has-feedback");
+        $('.rfc_input').hide().val("");
+        $('#nombreFiscalCliente').show();
+        $('#leyenda_filtro_cliente').html("Nombre:");
+      }
+      else if ($("#rbtn_rbtn_rfc").is(":checked")) {
+
+        $('#nombreFiscalCliente').hide().val("");
+        $('.rfc_input').show();
+        $('#leyenda_filtro_cliente').html("R.F.C.:");
+
+      }
+
+
       if( $("#opciones_filtros_por_cliente").is(":visible") ){
          $(this).html('Filtros por cliente <i class="fa fa-sort-down"></i>');
       }
